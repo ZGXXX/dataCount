b42d329408cab7254458ea8925a282f855107a00
zhaogx01 <zhaogx01@mingyuanyun.com>
2021-07-30 15:56:32 +0800

    '模拟本地读取写入'

--- client/client.go
+++ client/client.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"fmt"
 	pb "github.com/grpc-demo/datacount/protoc"
-	"google.golang.org/grpc"
 	"log"
 	"os"
 	"sync"
@@ -19,16 +18,16 @@ func main() {
 	t1 := time.Now()
 	var requestArr []string
 
-	conn, err := grpc.Dial(":50051", grpc.WithInsecure(), grpc.WithBlock())
-	if err != nil {
-		log.Fatalln(err)
-		fmt.Println("连接失败")
-	}
-	defer conn.Close()
+	//conn, err := grpc.Dial(":50051", grpc.WithInsecure(), grpc.WithBlock())
+	//if err != nil {
+	//	log.Fatalln(err)
+	//	fmt.Println("连接失败")
+	//}
+	//defer conn.Close()
 
 
 	// 本地待处理文件
-	localFile, err := os.Open("test.csv")
+	localFile, err := os.Open("source_0.csv")
 	if err != nil {
 		fmt.Println("打开文件失败")
 	}
@@ -38,11 +37,15 @@ func main() {
 	for fileScan.Scan() {
 		requestArr = append(requestArr, fileScan.Text())
 	}
+	//client := pb.NewCountServiceClient(conn)
+	t2 := time.Now()
+	joinSqlTime := t2.Sub(t1)
 
-	limit := 100000
+	limit := 5
 	ch := make(chan []string, 2000)
+	allCount := len(requestArr) / limit
+
 	go func() {
-		allCount := len(requestArr) / limit
 		for i:=0;i<=limit;i++ {
 			var newArr []string
 			if i == limit {
@@ -55,28 +58,42 @@ func main() {
 		close(ch)
 	}()
 
-	client := pb.NewCountServiceClient(conn)
-	for item:= range ch{
-		clientStream(client, item)
+	saveFile, err := os.OpenFile("统计文件.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln(err)
 	}
+	defer localFile.Close()
+	newWriter := bufio.NewWriter(saveFile)
+
+	countNum := 0
+	for items := range ch {
+		for _, item := range items {
+			newWriter.WriteString(item + "\n")
+			countNum ++
+		}
+	}
+	newWriter.Flush()
 
 
 	// 结束时间
-	t2 := time.Now()
-	joinSqlTime := t2.Sub(t1)
-	fmt.Printf("客户端发送花费时间：%f\n", joinSqlTime.Seconds())
+	t3 := time.Now()
+	joinSqlTime2 := t3.Sub(t1)
 
+	fmt.Printf("所有数据读取到数组花费：%f\n", joinSqlTime.Seconds())
+	fmt.Printf("客户端发送花费时间：%f\n", joinSqlTime2.Seconds())
+	fmt.Println(countNum)
 	//test()
 }
 
-func clientStream (client pb.CountServiceClient, data []string) {
+func clientStream (client pb.CountServiceClient, ch chan []string) {
+	defer wg.Done()
 	// 客户端流式请求
 	request, _ := client.ClientData(context.Background())
+	data := <-ch
 	err := request.Send(&pb.RowRequest{Line: data})
 	if err != nil {
 		fmt.Println("发送失败")
 	}
-
 	res, err := request.CloseAndRecv()
 	if err != nil {
 		fmt.Println(err)
